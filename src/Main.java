e9490de852dd5a3f9b669871ee0223977566c67f
sergey-v-sedov <sergey.v.sedov@gmail.com>
2023-06-13 21:03:52 +0300

    test

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
+import java.util.LinkedList;
+
 public class Main {
     public static void main(String[] args) {
-        ;
+
+        LinkedList<Integer> list = new LinkedList<>();
+        list.add(1);
+        list.add(2);
+        list.add(1, 3);
+
+        list.addFirst(4);
+        list.addLast(5);
+
+        list.set(2, 6);
+
+        int value = list.get(2);
+        System.out.println(value);
+
+        int first = list.getFirst();
+        System.out.println(first);
+
+        int last = list.getLast();
+        System.out.println(last);
+
+        list.remove(3);
+
+        Integer firstValueLink = list.removeFirst();
+        System.out.println(list);
+
+        System.out.println(firstValueLink);
+        System.out.println(firstValueLink);
+        System.out.println(firstValueLink);
     }
 }
